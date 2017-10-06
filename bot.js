9ffdd003b5b381e3b7c72af4a32f56e1ede504d9
William Forrest <wd4est@gmail.com>
2017-10-05 23:14:12 -0400

    Spicy bot quacks have a 5 second cooldown now. your welcome.

--- bot.js
+++ bot.js
@@ -1,9 +1,12 @@
-const Discord = require('discord.js');
+﻿const Discord = require('discord.js');
 const client = new Discord.Client();
 const settings = require('./settings.json');
 
+var userCooldowns = {};
+
 client.on('ready',() => {
     console.log('Online');
+    setInterval(function(){ for (userx in userCooldowns){if(userCooldowns[userx] > 0){userCooldowns[userx]-= 1;} }; }, 3000);
 })
 
 client.on('message', message => {
@@ -12,7 +15,8 @@ client.on('message', message => {
   }
 
   if(message.content === '!quack'){
-    if(message.member.voiceChannel){
+    if(message.member.voiceChannel && (userCooldowns[message.member.author.username] == undefined || userCooldowns[message.member.author.username] == 0)){
+      userCooldowns[message.member.author.username] = 5;
       message.member.voiceChannel.join()
       .then(connection => {
         const dispatcher = connection.playFile('C:/Users/Ryan/Documents/Quack-Sound-Effect.mp3');
@@ -24,7 +28,17 @@ client.on('message', message => {
       .catch(console.log);
     }
     else{
-      message.reply('You need to join a channel first!')
+      if (userCooldowns[message.member.author.username] != undefined && userCooldowns[message.member.author.username] == 0){
+          message.reply('Cooldown remaining: ' + userCooldowns[message.member.author.username]);
+      }
+      else{
+          message.reply('You need to join a channel first!');
+      }
+    }
+  }
+  if(message.content === ':duck:'){
+    if (message.member.author.bot == True){
+       message.delete(3).catch(console.error);
     }
   }
 });
